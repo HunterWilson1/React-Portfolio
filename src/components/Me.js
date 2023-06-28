3c2d7db0890c2078ab06bae3f3cbdd145f1f1358
Hunter Wilson <huntertheguy123@gmail.com>
2023-06-28 16:32:38 -0500

    link

--- src/components/Me.js
+++ src/components/Me.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { FaLinkedinIn, FaGithub, FaFileDownload } from "react-icons/fa";
+import { FaLinkedinIn, FaGithub, FaGoogle } from "react-icons/fa";
 import pic from "../images/grad.jpg";
-
 
 const Me = () => {
   const socials = [
@@ -24,13 +23,13 @@ const Me = () => {
       </h2>
       <h3 className="py-3 text-2xl">Full-Stack Web Developer</h3>
       <p className="max-w-xl mt-4 text-lg font-light">
-          I'm a full stack developer specializing in crafting dynamic web
-          applications with a keen eye for detail and a passion for
-          problem-solving. With a solid foundation in frontend and backend
-          technologies, I strive to deliver high-quality and user-friendly
-          solutions. My goal is to create seamless digital experiences that
-          captivate users and drive business growth.
-        </p>
+        I'm a full stack developer specializing in crafting dynamic web
+        applications with a keen eye for detail and a passion for
+        problem-solving. With a solid foundation in frontend and backend
+        technologies, I strive to deliver high-quality and user-friendly
+        solutions. My goal is to create seamless digital experiences that
+        captivate users and drive business growth.
+      </p>
 
       <div className="flex justify-evenly py-8 lg:py-16 text-3xl w-full md:w-1/3">
         {socials.map(({ id, link, icon }) => (
@@ -48,8 +47,18 @@ const Me = () => {
       </div>
 
       <div>
-        <img src={pic} alt="" className="w-60 h-60 md:w-72 md:h-72 object-cover object-top rounded-xl"/>
-        <a href="/Resume.docx" download={true} className="flex items-center justify-center mt-10 bg-rose-600 py-2 rounded-lg animate-bounce"><FaFileDownload />Resume </a>
+        <img
+          src={pic}
+          alt=""
+          className="w-60 h-60 md:w-72 md:h-72 object-cover object-top rounded-xl"
+        />
+        <a
+          href="https://docs.google.com/document/d/1U18Yk161hDFFpnHFBMpbycwAbLHqOOo50T-iCFiNpTw/edit?usp=sharing"
+          download={true}
+          className="flex items-center justify-center mt-10 bg-rose-600 py-2 rounded-lg animate-bounce"
+        >
+          <FaGoogle /> <span className="ml-1">Resume</span>
+        </a>
       </div>
     </section>
   );
